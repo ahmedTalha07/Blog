25cf1268b9e0681e98ded3fabbe836d305212b8f
Talha Ahmed <talhaadd7@gmail.com>
2025-09-21 14:49:46 +0500

    solving mysql issue

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "blogsite");
+$conn = mysqli_connect("
+ fc7ea64c7184800009e479091f98", "********", "blog_db");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
