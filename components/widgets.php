9335c1a10a9cd1fe3daecb46a9d653e795d3dad5
Talha Ahmed <talhaadd7@gmail.com>
2025-09-21 17:22:25 +0500

    just final steps

--- components/widgets.php
+++ components/widgets.php
@@ -4,7 +4,7 @@
     <div class="card mb-4">
         <div class="card-header">Search</div>
         <div class="card-body">
-            <form onsubmit="window.location.href='/search.php/' + encodeURIComponent(this.q.value); return false;">
+            <form action="search.php" method="get">
                 <div class="input-group">
                     <label for="q" class="visually-hidden">Search:</label>
                     <input class="form-control" type="text" id="q" name="q" placeholder="Search..." required>
