44a7153e8e8f61911713ba85b0a89cce45ba81db
Talha Ahmed <talhaadd7@gmail.com>
2025-09-21 14:54:45 +0500

    resolving mysql

--- db.php
+++ db.php
@@ -1,7 +1,16 @@
 <?php
-$conn = mysqli_connect("
- fc7ea64c7184800009e479091f98", "********", "blog_db");
+$host     = "db.fr-pari1.bengt.wasmernet.com";
+$port     = 10272; // Wasmer's port for your DB
+$dbname   = "blog_db";
+$username = "fc7ea64c7184800009e479091f98";
+$password = "068cfc7e-a64c-72cd-8000-a8aab7857d4a"; // put the password you got from Wasmer
+
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $dbname, $port);
+
+// Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
